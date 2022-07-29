e63c17c0cd1c5b90e5aa3055f561c104c71ef2f9
Glitch (glitch-hello-website) <none>
2022-07-29 15:42:02 +0000

    🎟🍲 Checkpoint ./scripts/jackpot-api.js:38221622/56 ./jackpot-api.html:38221622/573 ./scripts/jackpot-api-consts.js:38221622/678

---
+++ scripts/jackpot-api-consts.js
@@ -0,0 +1,4 @@
+const jackpotBaseURL = "http://www.google.com"
+const jackpotCompanyId = "04e094d2-69e6-4604-822f-fb52d702ce0c";
+const jackpotPolicyId = "73af114539b732e7a115b2420cda9db5";
+const jackpotAccessToken = "9e77969aafdabf8b5bb02c580ac1238a864a9d1e5d63715328952152d2e3a75dc0f8d5f7de5aeccb805e5421175e43b0b644ceabde63fd92eaa882054508bf147349404ff52224ef14808863dd2078145056e95d1566aa7cb903d532a31792dc9bb105a59d6dbb70cfa8c40c64b0b2e77f0e19403f88e224a08053e579b89598";
