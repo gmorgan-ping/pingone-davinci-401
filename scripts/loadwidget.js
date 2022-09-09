1960ee8a0360ad7b9afdbbe96c7280b0805410fd
Glitch (glitch-hello-website) <none>
2022-09-09 14:14:25 +0000

    ⛈⛴ Checkpoint ./scripts/loadwidget.js:38221622/106 ./202-1/index.html:38221622/55 ./202-1/completed.html:38221622/95 ./202-2/completed.html:38221622/2517

--- scripts/loadwidget.js
+++ scripts/loadwidget.js
@@ -1,10 +1,9 @@
-function loadwidget(policyId) {
+function loadwidget(policyId, renderComponent) {
   const tokenURL = "https://orchestrate-api.pingone.com";
   const flowURL = "https://auth.pingone.com/";
   const companyId = "ff4f6c34-2ed5-4210-9ad6-266b4f2412bd";
   const skApiKey =
     "f94829acebaec6f61c33fdf369a25adf6237c7e5f6142a7cbe5ba588ab134a01e30f43402446f1e5c591a90b073dbebe4d230af523cc4b2fc28ec7708a82dab323d4562554c470b4f6cebf0b256f55357ae46f1af27a15f8c9815bec37634644431451a4823c028eadc59390991ffcff7adb84e8f9288fd42ae7237c70e01cb4";
-  // const policyId = "6ce5990b898ee53eb49edb53e7c535ac";
 
   /*** Build the DaVinci Token URL. ***/
   const skGetTokenUrl = tokenURL + "/v1/company/" + companyId + "/sdktoken";
@@ -38,7 +37,7 @@ function loadwidget(policyId) {
       };
       /*** Invoke DaVinci Widget ****/
       console.log(props);
-      davinci.skRenderScreen(document.getElementById("widget"), props);
+      davinci.skRenderScreen(document.getElementById(renderComponent), props);
     })
     .catch((error) => console.log("error", error));
 
