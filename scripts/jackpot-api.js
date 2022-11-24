84b3ee9634f969b8fe9ac09ab20fdc400b35049a
Glitch (pingone-davinci-401) <none>
2022-11-24 13:56:38 +0000

    🚆📰 Checkpoint ./401-1/index.html:38221622/207 ./scripts/davinci-policy-constants.js:38221622/544 ./scripts/jackpot-api.js:38221622/457 ./scripts/loadwidget.js:38221622/580

--- scripts/jackpot-api.js
+++ scripts/jackpot-api.js
@@ -1,13 +1,10 @@
-const companyId = "858682c2-a8eb-4373-925d-55adea99df3c";
-const apiKey = "6b98aa4daa76381cd2842a56d6d8a6904d875f6a927d8002652a035c62f2a69b8066c9a6690ca69cc2313a6c10f91c75e17ae831ccb837a66181948677993afec59590a16a48d3df4e38bbc9fe2f91591bee275b3037c68986ae919b71d1310691246417abc08487173bff844301bf3cfe34c5936b072939c604bed4ca4f16f2";
 
 async function fetchJackpotData(policyId, callbackHandler) {
-  const flowURL = `https://orchestrate-api.pingone.com/v1/company/${DAVINCI_COMPANY_ID}/policy/${policyId}/start`;
+  const flowURL = `${DAVINCI_TOKEN_URL}/v1/company/${DAVINCI_COMPANY_ID}/policy/${policyId}/start`;
   console.log(`Flow URL: ${flowURL}`)
-  console.log(`X-SK-API-KEY: ${apiKey}`)
 
   const myHeaders = new Headers();
-  myHeaders.append("X-SK-API-KEY", apiKey);
+  myHeaders.append("X-SK-API-KEY", DAVINCI_POLICY_API_KEY);
   myHeaders.append("Content-Type", "application/json");
 
   var requestOptions = {
--- scripts/loadwidget.js
+++ scripts/loadwidget.js
@@ -1,16 +1,11 @@
 function loadwidget(policyId, renderComponent, flowInputVariables) {
-  const tokenURL = "https://orchestrate-api.pingone.com";
-  const flowURL = "https://auth.pingone.com/";
-  const companyId = "858682c2-a8eb-4373-925d-55adea99df3c";
-  const skApiKey =
-    "6b98aa4daa76381cd2842a56d6d8a6904d875f6a927d8002652a035c62f2a69b8066c9a6690ca69cc2313a6c10f91c75e17ae831ccb837a66181948677993afec59590a16a48d3df4e38bbc9fe2f91591bee275b3037c68986ae919b71d1310691246417abc08487173bff844301bf3cfe34c5936b072939c604bed4ca4f16f2";
 
   /*** Build the DaVinci Token URL. ***/
-  const skGetTokenUrl = tokenURL + "/v1/company/" + companyId + "/sdktoken";
+  const skGetTokenUrl = DAVINCI_TOKEN_URL + "/v1/company/" + DAVINCI_COMPANY_ID + "/sdktoken";
 
   //*** Add the API Key from your DaVinci Application. ***/
   var headers = new Headers();
-  headers.append("X-SK-API-KEY", skApiKey);
+  headers.append("X-SK-API-KEY", DAVINCI_POLICY_API_KEY);
 
   var requestOptions = {
     method: "GET",
@@ -25,9 +20,9 @@ function loadwidget(policyId, renderComponent, flowInputVariables) {
       var props = {
         config: {
           method: "runFlow",
-          apiRoot: flowURL,
+          apiRoot: DAVINCI_FLOW_URL,
           accessToken: responseData.access_token,
-          companyId: companyId,
+          companyId: DAVINCI_COMPANY_ID,
           policyId: policyId,
           parameters: flowInputVariables
         },
